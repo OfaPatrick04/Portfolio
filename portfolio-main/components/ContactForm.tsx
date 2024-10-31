937fd1f95ba98a69ce2afe261665088e15d359da
Ofa Partick <ofapatrick04@gmail.com>
2024-10-31 04:30:39 +0000

    form

--- portfolio-main/components/ContactForm.tsx
+++ portfolio-main/components/ContactForm.tsx
@@ -22,16 +22,16 @@ export function SignupFormDemo() {
         <div className="flex flex-col md:flex-row space-y-2 md:space-y-0 md:space-x-2 mb-4">
           <LabelInputContainer>
             <Label htmlFor="firstname">First name</Label>
-            <Input id="firstname" placeholder="Tyler" type="text" />
+            <Input id="firstname" placeholder="First Name" type="text" />
           </LabelInputContainer>
           <LabelInputContainer>
             <Label htmlFor="lastname">Last name</Label>
-            <Input id="lastname" placeholder="Durden" type="text" />
+            <Input id="lastname" placeholder="Last Name" type="text" />
           </LabelInputContainer>
         </div>
         <LabelInputContainer className="mb-4">
           <Label htmlFor="email">Email Address</Label>
-          <Input id="email" placeholder="projectmayhem@fc.com" type="email" />
+          <Input id="email" placeholder="email" type="email" />
         </LabelInputContainer>
 
         <button
