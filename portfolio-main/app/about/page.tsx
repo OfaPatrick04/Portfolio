7e0f1fe0e396fede3412bc28a044bdd5a6510da2
Ofa Partick <ofapatrick04@gmail.com>
2024-10-31 04:18:42 +0000

    new

--- portfolio-main/app/about/page.tsx
+++ portfolio-main/app/about/page.tsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import AboutHero from "@/components/AboutHero";
+import React from "react";
 
 const About = () => {
   return (
-    <div>About Page</div>
-  )
-}
+    <div>
+      <AboutHero />
+      <div className="max-w-5xl mx-auto px-8 relative">
+        <p className="text-sm md:text-base md:leading-loose tracking-wide">
+          With several years of experience in designing and developing
+          user-friendly, visually appealing websites, I’ve worked with clients
+          across various industries to bring their ideas to life
+        </p>
+      </div>
+    </div>
+  );
+};
 
-export default About
+export default About;
